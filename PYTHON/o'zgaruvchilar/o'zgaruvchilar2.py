66224aea372359dc74e6f75ffaf784dbe6a952b7
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-18 21:30:24 +0500

    updated

---
+++ PYTHON/o'zgaruvchilar/o'zgaruvchilar2.py
@@ -0,0 +1,26 @@
+# bir qator orqali ko'plab o'zgaruvchilarni elon qilish
+
+x, y, z, c = 17, 23, 28, 10
+print(x)
+print(y)
+print(z)
+print(c)
+
+
+# bitta qiymatni ko'plab o'zgaruvchilarga berish
+
+x = y = z = "Qizil"
+print(x, y, z)
+
+# List va boshqa ma'lumotlarni o'zgaruvchilarga berish
+
+mevalar = ["gilos", "uzum", "olcha"]
+x, y, z = mevalar
+print(x)
+print(y)
+print(z)
+
+mashinalar = ["Tesla", "Toyota", "Hundai", "Honda", "BYD"]
+
+a,b,c,d = mashinalar
+print(a,b,c,d)
