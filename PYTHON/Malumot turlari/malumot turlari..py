835aa009756d258fee73cbf605de0b092c32a043
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-18 21:35:44 +0500

    updated

---
+++ PYTHON/Malumot turlari/malumot turlari..py
@@ -0,0 +1,29 @@
+x = "Hello World"	# str	
+print(x)
+x = 20	# int	
+print(x)
+x = 20.5	# float	
+print(x)
+x = 1j	# complex	
+print(x)
+x = ["gilos", "uzum", "olcha"]	# list	
+print(x)
+x = ("Tesla", "Toyota", "Hundai", "Honda", "BYD")	# tuple	
+print(x)
+x = range(6)	# range	
+print(x)
+x = {"name" : "Islomjon", "age" : 18}	# dict	
+print(x)
+x = {"gilos", "uzum", "olcha"}	# set	
+print(x)
+x = frozenset({"gilos", "uzum", "olcha"})	# frozenset	
+print(x)
+x = True	# bool	
+print(x)
+x = b"Hello"	# bytes	
+print(x)
+x = bytearray(5)	# bytearray	
+print(x)
+x = memoryview(bytes(5))	# memoryview	
+print(x)
+x = None	# NoneType
