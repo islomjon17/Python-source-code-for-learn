c85b5b86798baaa7b580f36a7932c833e41fe9bb
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-18 21:21:36 +0500

    updated

---
+++ PYTHON/o'zgaruvchilar/o'zgaruvchilar.py
@@ -0,0 +1,50 @@
+# o'zgaruvchilarni yaratish
+
+x = 17
+y = "r"
+print(x)
+print(y)
+
+
+# o'zgaruvchilar uchun izohlar qoshish
+x = 17 # bu yerda x 17 ga teng bo'ldi
+x = "r" # endi esa x ni qiymaty "y" ga o'zgardi
+print(x)
+
+# o'zgaruvchi turlarni casting qilish
+
+x = int(17) # bu yerda 17 raqam
+x = str(23) # bu yerda  "23" matn holatida
+z = float(28) # bu yerda 28.0 onli kasr holatida
+
+# o'zgaruvchining ma'lumot turini olish
+
+x = 10
+y = "Tony"
+
+print(type(x))
+print(type(y))
+
+
+# O'ZGARUVCHI UCHUN NOMLAR BERISH
+
+myvar = "Tony"
+my_var = "Tony"
+_my_var = "Tony"
+myVar = "Tony"
+MYVAR = "Tony"
+myvar2 = "Tony"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+
+# MUMKIN ETILMAYDIGAN O'ZGARUVCHI UCHUN NOMLAR BERISH
+
+# 2myvar = "John"  O'ZGARUVCHI RAQAM BILAN BOSHLANMAYDI
+# my-var = "John" O'RTADA CHIZIQLARDAN FOYDALANILMAYDI
+# my var = "John" O'RTADA JOY TASHLAB YOZISH MUMKIN EMAS.
