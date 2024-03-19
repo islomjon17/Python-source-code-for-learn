bb78614ae2feb363b9d38afe2af1f25e0af2a061
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-19 21:31:26 +0500

    updated

---
+++ PYTHON/operatorlar/operator.py
@@ -0,0 +1,43 @@
+""" 
++ 	Qo'shish 	x + y 	
+- 	Ayrish 	x - y 	
+* 	Ko'paytirish 	x * y 	
+/ 	Bo'lish 	x / y 	
+% 	Modul 	x % y 	
+** 	kvadratga kotarish 	x ** y 	
+// 	 Qoldiqsiz bo'lish	x // y
+
+"""
+
+
+
+"""
+
+= 	    x = 5 	    x = 5 	
++= 	    x += 3 	    x = x + 3 	
+-=  	x -= 3 	    x = x - 3 	
+*= 	    x *= 3 	    x = x * 3 	
+/= 	    x /= 3 	    x = x / 3 	
+%= 	    x %= 3 	    x = x % 3 	
+//= 	x //= 3 	x = x // 3 	
+**= 	x **= 3 	x = x ** 3 	
+&= 	    x &= 3 	    x = x & 3 	
+|= 	    x |= 3 	    x = x | 3 	
+^= 	    x ^= 3 	    x = x ^ 3 	
+>>= 	x >>= 3 	x = x >> 3 	
+<<= 	x <<= 3 	x = x << 3
+"""
+x = 5 
+
+print(x + 3 ) # 8
+print(x - 3) # 2
+print(x * 3 ) # 15 
+print(x / 3 ) # 1.6666666666666667
+print(x % 3 ) # 2
+print(x // 3 ) #  1
+print(x ** 3) # 125
+print(x & 3) # 1
+print(x | 3) # 7
+print(x ^ 3 ) # 6
+print(x >> 3) # 0
+print(x << 3) # 40
