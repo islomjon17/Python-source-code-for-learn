1727a344509719a657ecbab9aad1e12787a171fe
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-19 20:45:10 +0500

    updated

---
+++ PYTHON/Booleans/Booleans.py
@@ -0,0 +1,27 @@
+"""
+Dasturlashda siz ko'pincha ifoda to'g'ri yoki noto'g'ri ekanligini bilishingiz kerak.
+
+Siz Python-da har qanday ifodani baholashingiz va ikkita javobdan birini olishingiz mumkin: True yoki False.
+"""
+
+print(10 > 9) # True
+print(10 == 9) # False
+print(10 < 9) # False
+
+
+
+### Aksariyat qadriyatlar haqiqatdir(Most Values are True)
+
+
+# print(bool("Hello"))
+# print(bool(15))
+
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
---
+++ PYTHON/String(matn)/string5.py
@@ -0,0 +1,9 @@
+# Python - String  metodlari
+
+matn = "Google's service, offered free of charge, instantly translates words, phrases, and web pages between English and over 100 other languages"
+print(matn)
+print(matn.capitalize())
+print(matn.casefold())
+print(matn.upper())
+print(matn.title())
+print(matn.encode())
