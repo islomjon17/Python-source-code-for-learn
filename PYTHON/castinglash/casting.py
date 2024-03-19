43ac43a2ca6e0125bd4fdd9154b94581b89cd65f
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-19 10:22:33 +0500

    updated

---
+++ PYTHON/castinglash/casting.py
@@ -0,0 +1,23 @@
+#Python Casting
+"""
+
+int() - butun sonli literaldan, float literalidan (barcha o'nli kasrlarni olib tashlash orqali) yoki satr literalidan (satr butun sonni ifodalagan holda) butun sonni yaratadi.
+    float() - butun son, float literal yoki satr literalidan float raqamini yaratadi (agar qator float yoki butun sonni bildirsa)
+    str() - qatorlar, butun sonlar va float literallari kabi turli xil ma'lumotlar turlaridan qatorni yaratadi
+"""
+
+# int
+x = int(1)   # natija 1
+y = int(2.8) # natija 2
+z = int("3") # natija 3
+# float
+x = float(1)     # natija 1.0
+y = float(2.8)   # natija 2.8
+z = float("3")   # natija 3.0
+w = float("4.2") #natija 4.2
+
+# string
+
+x = str("s1") # natija 's1'
+y = str(2)    # natija '2'
+z = str(3.0)  # natija '3.0' 
