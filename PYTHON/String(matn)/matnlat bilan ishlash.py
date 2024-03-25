e58d4e7a5069f909bdcae0977aa5214b290118e5
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-25 20:05:26 +0500

    updated

---
+++ PYTHON/String(matn)/matnlat bilan ishlash.py
@@ -0,0 +1,34 @@
+# ism =  "islomjon"
+# familya = "turg'unov"
+
+# toliq_ism = f"{ism} {familya}"
+# print(toliq_ism)
+
+
+# ism =  "Islomjon"
+# familya = "Turg'unov"
+
+# toliq_ism = f"{ism} {familya}"
+
+# print(toliq_ism.upper()) # hamma harflarni katta qilib beradi
+# toliq_ism = toliq_ism.upper()
+
+# print(toliq_ism.lower()) # Hamma harflarni kichkina qilib beradi
+
+# print(toliq_ism.title()) # Hamma harflarni brinchi harfini katta qilib beradi
+
+# print(toliq_ism.capitalize()) # Matnni birinchisida kelgan harfni katta qilib beradi.
+
+
+# telifon =  "                Redmi            "
+
+# print("Men ", telifon.lstrip().rstrip(), " ishlataman") # Chap tomondagi bosh joylarni olib tashlaydi
+# print("Men ", telifon.rstrip(), " ishlataman") # ong tomondagi bosh joylarni olib tashlaydi
+# print("Men ", telifon.lstrip().rstrip(), " ishlataman") # ikkala tomondagi bosh joylarni olib tashlaydi
+# print("Men ", telifon.strip(), " ishlataman") # ikkala tomondagi bosh joylarni olib tashlaydi
+
+
+
+# ism = input(str("Imingizni kiriting:  "))
+
+# print("Salom ", ism.title())
---
+++ PYTHON/operatorlar/arifmetik operatorlar2.py
@@ -0,0 +1,11 @@
+# print(2*4+10)
+# print(19/5)
+# print(20/5)
+# print(16//4)
+# print(15//4)
+# print(" ", 15%4)
+# print(2**4)
+# print("55 ning kvadrati ", 55**2, " ga teng")
+# print("3+7 ", 3+7, " ga teng")
+
+
